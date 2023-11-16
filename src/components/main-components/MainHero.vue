0ba6304fb203c7fb711ac4c2603bbea28a524139
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-16 11:05:22 +0100

    Add: hero foodie journal base-scheme

--- src/components/main-components/MainHero.vue
+++ src/components/main-components/MainHero.vue
@@ -11,8 +11,17 @@
                     <p>Food Corner: Top Japanese Restaurants for Sushi</p>
                     <p class="hero__text-date">March 25th, 2019</p>
                     <p class="hero__badge">today's pick</p>
+                </div>                
+            </div>
+        </div>
+        <div class="container">
+            <div class="hero__journal">
+                <h3>Foodie Journal</h3>
+                <div>
+                    <img src="/single-post-img3-400x263.jpg" alt="">
+                    <h5>Food Corner</h5>
+                    <small>by...</small>
                 </div>
-                
             </div>
         </div>
     </div>
