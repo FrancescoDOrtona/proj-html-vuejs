22d6b9cf4a84e64a9cfc4ab6852d43f69a2a5404
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-17 20:10:36 +0100

    Add: Footer subscribe section

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,11 +3,72 @@
 </script>
 
 <template>
-    <div>
-        Footer
+    <div class="footer">
+        <div class="footer__bg">
+            <div class="container">
+                <div class="footer__subscribe">            
+                    <div class="subscribe__title">
+                        <h2>Subscribe & Receive A Free eBook</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, perferendis quos nesciunt sint numquam sit!</p>
+                    </div>            
+                    <div class="subscribe__email">
+                        <input class="subscribe__input" type="email" placeholder="Insert your email..">
+                        <button class="subscribe__btn">Subscribe</button>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                </div>     
+            </div>
+        </div>
+                      
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+@use '../style/partials/variables' as *;
+
+.footer__bg{
+    background-image: url('/subscribe-sec-bg.png');
+    background-color: white;
+    background-position: center;
+    background-size: cover;
+    min-height: 400px;
+    display: flex;
+    align-items: center;
+}
+.footer__subscribe{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+}
+
+.subscribe__title{
+    text-align: center;
+}
+
+.subscribe__title h2{
+    margin-bottom: 20px;
+}
+
+.subscribe__email{
+    width: 100%;
+    padding: 0px 50px;
+    display: flex;
+    gap: 20px;
+}
+
+.subscribe__btn{
+    padding: 10px 50px;
+    background-color: $orange;
+    border-radius: 3px;
+    color: white;
+    text-transform: uppercase;
+}
+
+.subscribe__input{
+    width: 100%;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    padding: 0px 10px;
+}
 </style>
